94c75c1bebab203cd8c4127667bb5183156e3a6a
R3x <Adeosunolamide202@gmail.com>
2023-06-16 15:29:01 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a-z in small
+ * 	letters.
+ * Return: always 0.
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter='a', letter <= z, letter++)
+		putchar(letter);
+	return (0);
+}
