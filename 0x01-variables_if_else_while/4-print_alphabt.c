3d36837258d0f36dfc59327ab79d7d6a31f6b223
R3x <Adeosunolamide202@gmail.com>
2023-06-16 16:49:59 -0700

    al

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,7 +3,7 @@
 /**
  * main - prints all alphabets
  *	except q and e.
- * Return: always 0;
+ * Return: always 0.
 */
 
 int main(void)
@@ -20,6 +20,6 @@ int main(void)
 
 		putchar('\n');
 
-		return(0);
+		return (0);
 	}
 }
