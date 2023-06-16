0f167d856c8c039c4cce3a03f981aa886467d9ab
R3x <Adeosunolamide202@gmail.com>
2023-06-15 22:10:47 -0700

    Done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Print "with proper grammar, but the outcome is 
+ * main - Print "with proper grammar, but the outcome is
  * a piece of art," followed by a new line.
  * Return: always 0
 */
