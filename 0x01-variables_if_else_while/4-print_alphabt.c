b560a3615ced6e2bfb37a8c74abd376999077044
R3x <Adeosunolamide202@gmail.com>
2023-06-16 16:42:12 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints all alphabets
+ *	except q and e.
+ * Return: always 0;
+*/
+
+int main(void)
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		if (al == 'q' || al == 'e')
+		{
+			continue;
+			putchar(al);
+		}
+
+		putchar('\n');
+
+		return (0);
+	}
+}
