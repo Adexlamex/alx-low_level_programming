48580dc8ca945ec72942d71aa4628148f6951767
R3x <Adeosunolamide202@gmail.com>
2023-06-15 22:02:35 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,13 @@
-/**
- * File: 5-printf.c
- * Author: Adexlamex
-*/
-
 #include <stdio.h>
 
 /**
- * Main - Print "with proper grammar, but the outcome is a piece of art," followed by a new line
- * Return always 0
+ * Main - Print "with proper grammar, but the outcome is 
+ * 		a piece of art," followed by a new line.
+ * Return: always 0
 */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
