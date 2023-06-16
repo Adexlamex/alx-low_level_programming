338fe851e3cfed7c0a2dd2642ac910c2eec1c35a
R3x <Adeosunolamide202@gmail.com>
2023-06-15 22:42:34 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,14 +2,14 @@
 
 /**
  * main - Prints sizes of variable
- * 	types.
+ *	types.
  * Return: always 0.
 */
 
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of a int: %zu byte(s)\n", sizeof(char));
+	printf("Size of a int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
