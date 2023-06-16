76cdde0a0f4376e3dca9dc59f21dab05c84e5937
R3x <Adeosunolamide202@gmail.com>
2023-06-15 21:53:06 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,17 @@
-#!/bin/bash
-printf ("with proper grammar, but the outcome is a piece of art,\n");
+/**
+ * File: 5-printf.c
+ * Author: Adexlamex
+*/
+
+#include <stdio.h>
+
+/**
+ * Main - Print "with proper grammar, but the outcome is a piece of art" followed by a new line
+ * Return always 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return(0);
+}
