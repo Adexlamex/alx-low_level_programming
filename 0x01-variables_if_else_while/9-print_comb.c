cb35d7aa5db308dbfab77149c4b05d35848f8914
R3x <Adeosunolamide202@gmail.com>
2023-06-16 17:57:39 -0700

    num

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints combinations of
+ *	single digits.
+ * Return: always 0.
+*/
+
+int main(void)
+{
+	int num;
+	for (num = 0; num < 10; num++)
+	{
+		putchar(numnum);
+	}
+	putchar(,);
+	return (0);
+}
