ce5d35baef8ee07d38c0012aef8dce0efcb8d38c
R3x <Adeosunolamide202@gmail.com>
2023-06-16 18:03:38 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,8 +10,14 @@ int main(void)
 {
 	int num;
 	for (num = 0; num < 10; num++)
-	{
-		putchar(numnum);
+	{	
+		putchar((num % 10) + '0');
+
+		if (num == 9)
+		continue;
+		
+		putchar(',');
+		putchar(' ');
 	}
 	putchar(,);
 	return (0);
