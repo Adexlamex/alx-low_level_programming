c3f0c18fe0e02fd57b00dbd8afe45e7411265268
R3x <Adeosunolamide202@gmail.com>
2023-06-20 14:48:02 -0700

    is

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,7 @@
 /**
  * _islower - prints the characteristic
  *	of the letter.
+ * c - an integer declared.
  * Return: always 0.
 */
 
@@ -11,8 +12,8 @@ int _islower(int c)
 	if (c >= 'a' && c <= 'z')
 
 	{
-		return(1);
+		return (1);
 	}
 	else
-		return(0);
+		return (0);
 }
