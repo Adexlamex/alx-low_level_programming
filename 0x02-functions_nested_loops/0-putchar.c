808bcb893d95a5f595d683519aae7e7bb40a21d4
R3x <Adeosunolamide202@gmail.com>
2023-06-20 12:14:29 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,8 +8,7 @@
 
 int main(void)
 {
-	char;
-	_putchar('_');
+	_putchar(_);
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -17,6 +16,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
