505507e5ddf3b2a68f23964a3e9cb968a0281e64
R3x <Adeosunolamide202@gmail.com>
2023-06-20 13:46:58 -0700

    Done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,14 @@
  * Return: always 0.
 */
 
-void print_alphabet(void)
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
+
+void print_alphabet(void) 
 {
 	char letter;
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++ )
 	{
 		_putchar(letter);
 	}
