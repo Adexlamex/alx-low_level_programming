e044df6a2eaf31876cfb4f6c522e48089259e647
R3x <Adeosunolamide202@gmail.com>
2023-06-22 09:07:33 -0700

    Done

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonally.
+ * @n - number of times a character
+ *	is printed.
+ */
+
+void print_diagonal(int n)
+{
+	int l, sp;
+
+	if (n > 0)
+	{
+		for (l = 0; l < n; l++)
+		{
+			for (sp = 0; sp < l; sp++)
+				_putchar(' ');
+			_putchar('\\');
+
+			if (l == n - 1)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
