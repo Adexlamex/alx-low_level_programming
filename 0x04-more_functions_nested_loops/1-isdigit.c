45d92dd478a16cf873cf0a0d3fc1f234275a0dac
R3x <Adeosunolamide202@gmail.com>
2023-06-22 04:48:55 -0700

    1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,12 +3,13 @@
 /**
  * _isdigit - checks for a digit.
  * @c: the integer written.
-*/
+ * Return: always 0.
+ */
 
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
-		return(1);
+		return (1);
 	else
-		return(0);
+		return (0);
 }
