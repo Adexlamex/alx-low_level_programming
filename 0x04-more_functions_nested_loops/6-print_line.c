4bb17bd87f050c3ef3c4d3f5f4a5c3952c4deffe
R3x <Adeosunolamide202@gmail.com>
2023-06-22 08:54:40 -0700

    Done

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line.
+ * @n:is the number of times _ is printed.
+ * _ - character printed.
+ * Return: always 0.
+ */
+
+void print_line(int n)
+{
+	int l;
+
+	if (n > 0)
+	{
+		for (l = 0; l < n; l++)
+			_putchar('_');
+	}
+	else 
+		_putchar('\n');
+}
