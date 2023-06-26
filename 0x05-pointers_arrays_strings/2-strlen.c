7d3ac1ab5633d072d4da26268e8c47e9b3a107e7
R3x <Adeosunolamide202@gmail.com>
2023-06-26 14:16:35 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - prints the length of strings.
+ * @s is the pointed string.
+ *
+ * Return: always nothing
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
