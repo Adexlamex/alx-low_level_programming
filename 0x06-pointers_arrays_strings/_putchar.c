3d16c55f8724b7ba8048c3f792b035350198bd5a
R3x <Adeosunolamide202@gmail.com>
2023-06-29 10:58:39 -0700

    0

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c
+ *	to stdout.
+ * @c: the character to be writen.
+ *
+ * Return: 1 on Success.
+ * -1, on error.
+ */
+
+int _putchar(char c)
+{
+	return (1, &c, 1);
+}
