c1900a8e41fa73e836baaa444b33604d2ba15d06
R3x <Adeosunolamide202@gmail.com>
2023-07-05 11:24:52 -0700

    0

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: Main header for 0x08-Recursive tasks.
+ * Author: Olamide Adeosun.
+ *
+ * Description: Main header containing prototypes for 0x08-Recursive
+ *	tasks.
+ */
+
+int _putchar(char c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+void _puts_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
+#endif
