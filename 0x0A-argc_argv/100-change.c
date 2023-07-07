8d3a48e7dfb7b217128644ea1a673434d11359d7
R3x <Adeosunolamide202@gmail.com>
2023-07-07 11:19:52 -0700

    100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,34 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-/**
- * main - program that prints its name, followed by a new line
- * @argc: argument that counts argument input
- * @argv: argument that stores the strings in an array of char* (strings)
- * Return: 0
- */
-int main(int argc, char *argv[])
-{
-	int sum, val, i;
 
-	sum = 0;
-	if (argc < 1)
-		printf("%d\n", 0);
-	while (argc-- && argc > 0)
-	{
-		for (i = 0; argv[argc][i] != '\0'; i++)
-		{
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Error\n");
+        return 1;
+    }
 
-			if (!(isdigit(argv[argc][i])))
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		val = atoi(argv[argc]);
-		sum += val;
-	}
-	printf("%d\n", sum);
-	return (0);
+    int cents = atoi(argv[1]);
+
+    if (cents < 0) {
+        printf("0\n");
+        return 0;
+    }
+
+    int coins[] = {25, 10, 5, 2, 1};
+    int num_coins = 0;
+    int i;
+
+    for (i = 0; i < sizeof(coins) / sizeof(coins[0]); i++) {
+        num_coins += cents / coins[i];
+        cents %= coins[i];
+    }
+
+    printf("%d\n", num_coins);
+
+    return 0;
 }
+
