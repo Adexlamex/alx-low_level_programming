15e3493c4e869d278905295965ee695082161e8a
R3x <Adeosunolamide202@gmail.com>
2023-07-10 10:31:10 -0700

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,19 +11,24 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *my_array;
+	char *p;
 	unsigned int i = 0;
 
-	my_array = malloc(size * sizeof(c));
+	if (size == 0)
+		return (NULL);
+
+	p = (char *) malloc(sizeof(char) * size);
+
+	if (p == NULL)
+		return (0);
 
 	while (i < size)
 	{
-		if (my_array == NULL)
-		{
-			return (NULL);
-		}
-		my_array[i] = c;
+		*(p + i) = c;
 		i++;
 	}
-	return (my_array);
+
+	*(p + i) = '\0';
+
+	return (p);
 }
